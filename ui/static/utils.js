0351f3bcd7266ce56b96e51d5ad97f3441c66183
Matti Nordström <matti.nordstrom@gmail.com>
2024-09-05 20:26:10 +0200

    Better error handling in safelyParseJSON

--- ui/static/utils.js
+++ ui/static/utils.js
@@ -6,6 +6,13 @@ const safelyParseJSON = (data) => {
     try {
         return JSON.parse(data);
     } catch (e) {
+        const errorMsg = "FRONTEND ERROR! Could not parse json. Returning data as " + typeof data + " ("+e+")";
+
+        console.error(errorMsg);
+        
+        const logMsg = '<span class="square square-red"></span> ' + errorMsg + '<br />';
+        document.getElementById("footer-log").insertAdjacentHTML('afterbegin', logMsg);
+
         return data;
     }
 }
