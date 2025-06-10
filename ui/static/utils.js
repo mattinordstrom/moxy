c9c79cb28195d24a432f7f26a55165c5f5389618
Matti Nordström <mattinordstrom@proton.me>
2025-06-10 08:29:36 +0200

    Minor fix in safelyParseJSON

--- ui/static/utils.js
+++ ui/static/utils.js
@@ -1,12 +1,17 @@
 const safelyParseJSON = (data) => {
-    if (typeof data === 'number' || data === '') {
+    if (data === "null") return null;
+    if (data === "true") return true;
+    if (data === "false") return false;
+
+    const couldBeJSON = typeof data === 'string' && (data.trim().startsWith('{') || data.trim().startsWith('['));
+    if (typeof data === 'number' || data === '' || !couldBeJSON) {
         return data;
     }
 
     try {
         return JSON.parse(data);
     } catch (e) {
-        const errorMsg = "FRONTEND ERROR! Could not parse json. Returning data as " + typeof data + " ("+e+")";
+        const errorMsg = "FRONTEND ERROR! Could not parse json. " + typeof data + " ("+e+")";
 
         console.error(errorMsg);
         
